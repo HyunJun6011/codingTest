5ffe492b1c2f8038175f0c10b50648d19794c89a
hyunjun <y.raymond0211@gmail.com>
2025-11-29 03:18:57 +0900

    Update 직각삼각형.java
    처음 푼 풀이에 대한 오답노트

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -21,3 +21,33 @@ public class Main {
         }
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 3개의 변 입력받기
+        while(true) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+
+            if((a==0) && (b==0) && (c==0)) break;
+            String s = " ";
+            if (c * c == a * a + b * b) {
+                s = "right";
+                System.out.println(s);
+            } else {
+                s = "wrong";
+                System.out.println(s);
+            }
+        }
+    }
+}
+
+// 3개의 변 중에서 항상 c의 길이가 길다고 해서 틀림.
+  10 6 8 와 같은 입력이 들어오면 실제로는 직각삼각형이나 wrong을 출력함.
+  따라서 배열에 저장해두고 정렬을 해서 풀면 쉬움,
+*/
